b2b4da0ae05df41eed58837ef7f07a756b227ac6
jeriks1223 <jerectemaz@gmail.com>
2023-02-05 00:07:50 +0800

    final_output

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,11 @@
 <template>
-  <Tutorial/>
+  <LoginPage/>
 </template>
 
 <script>
+import LoginPage from '../components/LoginPage.vue';
 export default {
-  name: 'IndexPage'
+    name: "IndexPage",
+    components: { LoginPage }
 }
 </script>
